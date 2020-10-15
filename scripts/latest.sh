a82892cb444197af536cd13b9889ef49b6ea93d5
Antoine Leblanc <antoine@hasura.io>
2020-10-15 19:27:47 +0100

    add mac os support

--- scripts/latest.sh
+++ scripts/latest.sh
@@ -5,11 +5,19 @@ set -e
 
 PACKAGE=stylish-haskell
 FORK=hasura
+OS=''
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    OS="linux"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    OS="darwin"
+fi
+
 echo Downloading and running $PACKAGE from $FORK...
 
 RELEASES=$(curl --silent https://github.com/$FORK/$PACKAGE/releases)
-URL=https://github.com/$(echo $RELEASES | grep -o '\"[^\"]*-linux-x86_64\.tar\.gz\"' | sed s/\"//g | head -n1)
-VERSION=$(echo $URL | sed -e 's/.*-\(v[\.0-9]\+-linux-x86_64\)\.tar\.gz/\1/')
+URL=https://github.com/$(echo $RELEASES | grep -o '\"[^\"]*-'$OS'-x86_64\.tar\.gz\"' | sed s/\"//g | head -n1)
+VERSION=$(echo $URL | sed -e 's/.*-\(v[\.0-9]\+-'$OS'-x86_64\)\.tar\.gz/\1/')
 TEMP=$(mktemp --directory .$PACKAGE-XXXXX)
 
 cleanup(){
